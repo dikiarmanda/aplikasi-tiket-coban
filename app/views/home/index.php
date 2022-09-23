b330606e8de2e874474b2d8ddbe67ed0eec7a3c1
tarajanuarr <taranatassa25@gmail.com>
2022-09-23 20:48:56 +0700

    css di home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,19 +1,12 @@
 <div class="container text-center py-5 my-5">
-    <div class="container mt-5 rounded-4 mt-3">
-    <div class="row justify-content-center">
-                    <div class="card bg-opacity-50" style="width: 30rem:"> 
-          <div class="card-header">
-            <span class="placeholder col-12 bg-primary"></span>
-            <h5 class="card">Featured</h5>
-          </div>
-          <div class="card-body py-5">
-            <h5 class="card-title">Tiket</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Tiket</a>
-            <a href="#" class="btn btn-primary">Laporan</a>
-            <a href="#" class="btn btn-primary">Statistik</a>
-          </div>
-        </div>
-         </div>
-     </div>
- </div>
+    <div class="bg-opacity-50" style="width: 30rem:">
+    <div class="home">
+        <h1> Coban Binangun</h1>
+    <div>
+        <button type="button">Tiket</button>
+        <button type="button">Laporan</button>
+        <button type="button">Statistik</button>
+    </div>
+</div>
+</div>
+</div>
