6e6305caf060a6954beea2fea361dec70e691d94
tarajanuarr <taranatassa25@gmail.com>
2022-10-24 18:43:54 +0700

    aplikasi tiket -> tiket

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,6 +1,6 @@
 <div class="container text-center py-5 my-5">
     <div class="row w-50 bg-light bg-opacity-50 rounded-4 py-3 mt-3 mx-auto">
-        <h1 class="display-2">Aplikasi Tiket</h1>
+        <h1 class="display-2">Tiket</h1>
         <h1 class="display-2">Coban Binangun</h1>
     </div>
     <div class="row justify-content-center mt-5">
